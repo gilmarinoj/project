c236dcb2af15cefc5e9c9192ee71cb9c895f8bcb
gilmarinoj <gilmarinoj@gmail.com>
2024-11-09 21:09:10 -0400

    first commit

---
+++ views/auth/registro.php
@@ -0,0 +1,59 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo ?></h2>
+    <p class="auth__texto">Registrate en EventConnect</p>
+
+
+    <form class="formulario">
+    <div class="formulario__campo">
+            <label for="nombre" class="formulario__label">Nombre</label>
+            <input
+                type="text"
+                class="formulario__input"
+                placeholder="Tu Nombre"
+                id="nombre"
+                name="nombre">
+        </div>
+        <div class="formulario__campo">
+            <label for="apellido" class="formulario__label">Apellido</label>
+            <input
+                type="text"
+                class="formulario__input"
+                placeholder="Tu Apellido"
+                id="apellido"
+                name="apellido">
+        </div>
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Email</label>
+            <input
+                type="email"
+                class="formulario__input"
+                placeholder="Tu Email"
+                id="email"
+                name="email">
+        </div>
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Contrasena</label>
+            <input
+                type="password"
+                class="formulario__input"
+                placeholder="Tu Contrasena"
+                id="password"
+                name="password">
+        </div>
+        <div class="formulario__campo">
+            <label for="password2" class="formulario__label">Repetir Contrasena</label>
+            <input
+                type="password"
+                class="formulario__input"
+                placeholder="Repetir Contrasena"
+                id="password2"
+                name="password2">
+        </div>
+        <input type="submit" class="formulario__submit" value="Crear Cuenta">
+    </form>
+
+    <div class="acciones">
+        <a href="/login" class="acciones__enlace">Ya tienes cuenta? Inicia Sesion!</a>
+        <a href="/olvide" class="acciones__enlace">Olvidaste tu contrasena?</a>
+    </div>
+</main>
---
+++ views/templates/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__contenido">
+            <h3 class="footer__logo">
+                &#60;EventConnect />
+            </h3>
+
+            <p class="footer__texto">
+                EventConnect es una conferencia para desarrolladores de todos los niveles, se lleva a cabo de forma presencial y en línea.
+            </p>
+        </div>
+
+        <nav class="menu-redes"> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://facebook.com/gilmarinoj">
+                <span class="menu-redes__ocultar">Facebook</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com/gilmarinoj">
+                <span class="menu-redes__ocultar">Twitter</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://youtube.com/gilmarinoj">
+                <span class="menu-redes__ocultar">YouTube</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://instagram.com/gilmarinoj">
+                <span class="menu-redes__ocultar">Instagram</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://tiktok.com/@gilmarinoj">
+                <span class="menu-redes__ocultar">Tiktok</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/gilmarinoj">
+                <span class="menu-redes__ocultar">GitHub</span>
+            </a>
+        </nav>
+    </div>
+
+    <p class="footer__copyright">
+        EventConnect
+        <span class="footer__copyright--regular">
+            - Todos los derechos reservados <?php echo date('Y'); ?>
+        </span>
+    </p>
+</footer>
